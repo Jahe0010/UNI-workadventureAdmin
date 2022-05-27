e5426f917394c230ed4594a6fb1218e62972742b
Your Name <you@example.com>
2022-05-27 12:43:02 +0200

    more tests

--- backend/api/room.py
+++ backend/api/room.py
@@ -4,25 +4,21 @@ router = APIRouter(prefix="/admin/api/room")
 
 
 @router.get("/access")
-async def room_access(request: Request):
+async def room_access(userIdentifier, playUri, ipAddress):
     """
     The room-access-endpoint. It returns a static JSON.
     """
-    print(request.json())
-    print(request.headers)
-    print(request.query_params)
+    print(userIdentifier)
+    print(playUri)
+    print(ipAddress)
+    
     return {
-        "mapUrl": "play.hs-kl.de/maps/zw/zw.json",
-        "policy_type": 1,
-        "tags": [],
-        "authenticationMandatory": False,
-        "roomSlug": None,
-        "contactPage": None,
-        "group": None,
-        "iframeAuthentication": None,
-        "miniLogo": None,
-        "loadingLogo": None,
-        "loginSceneLogo": None,
-        "showPoweredBy": True,
-        "loadingCowebsiteLogo": None
+        "email": None,
+        "userUuid": userIdentifier,
+        "tags": ["admin"],
+        "visitCardUrl": None,
+        "textures": [],
+        "messages": [],
+        "anonymous": True,
+        "userRoomToken": ""
     }
