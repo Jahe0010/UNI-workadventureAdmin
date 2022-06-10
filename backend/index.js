8ee3bc0ca9a4c250a03b66ecdcb0c9971882cda2
Jannick Heim <jannickheim@yahoo.de>
2022-06-10 15:33:46 +0200

    fix bug

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.get("/admin/api/map", (req, res) => {
             tags: [],
             authenticationMandatory: false,
             roomSlug: null,
-            contactPage: "/Einstiegsseite/credits.html",
+            contactPage: "Einstiegsseite/credits.html",
             group: "wa",
             iframeAuthentication: null,
             miniLogo: null,
