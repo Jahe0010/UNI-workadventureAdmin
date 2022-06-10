6ac0c8c47b89f5bd015cea0617303feb9e3f3b33
Jannick Heim <jannickheim@yahoo.de>
2022-06-10 13:35:26 +0200

    changes

--- backend/db/dbSelection.js
+++ backend/db/dbSelection.js
@@ -5,14 +5,16 @@ const mysql = require("./dbConnection.js");
  * @param {} playerUUID 
  * @returns 
  */
-async function isAdmin(playerUUID) {
-    try {
-        let result = mysql.mysqlConnection.query("SELECT id FROM admins WHERE userId = '" + playerUUID + "'");
-        return result.length > 0 ? true : false;
-    } catch (err) {
-        console.log("an error occured while selecting from the database " + err)
-        return false;
-    }
+function isAdmin(playerUUID) {
+    return new Promise((resolve, reject)=>{
+        mysql.mysqlConnection.query("SELECT id FROM admins WHERE userId = '" + playerUUID + "'", function (err, result) {
+            if (err) {
+                console.log("an error occured while selecting from the database " + err)
+                return reject(false);
+            };
+            return resolve(result.length > 0 ? true : false);
+        });
+    });
 }
 
 module.exports = { 
