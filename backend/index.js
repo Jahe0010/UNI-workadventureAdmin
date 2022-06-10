6bc7b5ae702af24facb269ecdbe30019a9b4c0dd
Jannick Heim <jannickheim@yahoo.de>
2022-06-10 08:34:05 +0200

    finish code documentation

--- backend/index.js
+++ backend/index.js
@@ -1,51 +1,28 @@
+const wokaList = require('./resources/wokaList.json');
+const express = require("express")
+const bodyParser = require("body-parser");
+
 const dbInsert = require("./db/dbInsert.js");
 const dbSelection = require("./db/dbSelection.js");
 const dbDelete = require("./db/dbDelete.js");
+const utils = require("./shared/utils.js");
 
-const express = require("express")
-const bodyParser = require("body-parser");
+// initialize express app
 const app = express();
-require('dotenv').config();
-const wokaList = require('./resources/wokaList.json');
 
+// make use of bodyParser Plugin to parse incoming bodys
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//Extract textures from the woka list
-function getAllTextures() {
-    let wokas = []
-    Object.keys(wokaList).forEach(groupName => {
-        let group = wokaList[groupName]
-        group.collections.forEach(collection => {
-            collection.textures.forEach(texture => {
-                wokas.push({
-                    id: texture.id,
-                    url: texture.url,
-                    layer: groupName
-                })
-            })
-        })
-    })
-    return wokas
-}
-
-// check if the incoming pw matches the example pw
-function isAuthenticated(token) {
-    const adminPW = process.env.AdminPassword;
-
-
-    if(token) {
-        return ((token == "Bearer " + adminPW) || (token == adminPW));
-    }
-
-    return false;
-}
-
-//returns the map object. this includes the url of the map that has to be loaded
+/**
+ * 401 - unauthorized
+ * 200 - returns the map object (standard endpoint read more about it on workadventure)
+ */
 app.get("/admin/api/map", (req, res) => {
-    if (isAuthenticated(req.header('authorization'))) {
+    if (utils.isAuthenticated(req.header('authorization'))) {
+        // we extract the map url based on the incoming playUri
         res.send(JSON.stringify({
-            mapUrl : "https://play.hs-kl.de/maps/" + req.query.playUri.split("maps/")[1],
+            mapUrl : "maps/" + req.query.playUri.split("maps/")[1],
             policy_type: 1,
             tags: [],
             authenticationMandatory: false,
@@ -65,27 +42,34 @@ app.get("/admin/api/map", (req, res) => {
     res.sendStatus(401)
 })
 
-//returns a list of all available wokas
+/**
+ * 401 - unauthorized
+ * 200 - returns a list of all available wokas (standard endpoint read more about it on workadventure)
+ */
 app.get("/admin/api/woka/list", (req, res) => {
     // You receive the userId
-    if (isAuthenticated(req.header('authorization'))) {
+    if (utils.isAuthenticated(req.header('authorization'))) {
         res.send(wokaList)
     }
     res.sendStatus(401)
 })
 
-// returns the information about a user and his rights when he tries to access a room
+/**
+ * 401 - unauthorized
+ * 200 - returns all information about a user who wants to access a room(standard endpoint read more about it on workadventure)
+ */
 app.get("/admin/api/room/access", (req, res) => {
-    if (isAuthenticated(req.header('authorization'))){
+    if (utils.isAuthenticated(req.header('authorization'))){
         console.debug("Receive access request with identifier:", req.query.userIdentifier)
         let characterLayers = req.query.characterLayers || []
 
         // Notice that we filter the textures based on the user selection (given on characterLayers)
-        let textures = getAllTextures().filter(woka => characterLayers.indexOf(woka.id) !== -1)
+        let textures = utils.getAllTextures().filter(woka => characterLayers.indexOf(woka.id) !== -1)
 
         // make sure to preserve the texture order (given on characterLayers)
         textures.sort( (t1, t2) => characterLayers.indexOf(t1.id) - characterLayers.indexOf(t2.id) )
 
+        // we check if a incoming user is an admin
         let user_tag = dbSelection.isAdmin(req.query.userIdentifier) ? "admin" : "user"
         res.send(
             JSON.stringify({
@@ -105,8 +89,9 @@ app.get("/admin/api/room/access", (req, res) => {
 /**
  * Tags a Player as an admin
  * Requires playerUuid
- * 200 ok - if successfull
+ * 200 ok and an additional success message - if successfull
  * 500 bad request - on an error -> check logs!
+ * 401 unauthorized 
  */
 app.post('/admin/api/setAdmin', (req,res) => {
     if (isAuthenticated(req.header('authorization'))){
@@ -125,6 +110,7 @@ app.post('/admin/api/setAdmin', (req,res) => {
  * Requires playerUuid
  * 200 ok - if successfull
  * 500 bad request - on an error -> check logs!
+ * 401 unauthorized 
  */
 app.delete('/admin/api/removeAdmin', (req,res) => {
     if (isAuthenticated(req.header('authorization'))){
---
+++ backend/shared/utils.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const wokaList = require('../resources/wokaList.json');
+
+//Extract textures from the woka list
+function getAllTextures() {
+    let wokas = []
+    Object.keys(wokaList).forEach(groupName => {
+        let group = wokaList[groupName]
+        group.collections.forEach(collection => {
+            collection.textures.forEach(texture => {
+                wokas.push({
+                    id: texture.id,
+                    url: texture.url,
+                    layer: groupName
+                })
+            })
+        })
+    })
+    return wokas
+}
+
+// check if the incoming pw matches the example pw
+function isAuthenticated(token) {
+    const adminPW = process.env.AdminPassword;
+
+
+    if(token) {
+        return ((token == "Bearer " + adminPW) || (token == adminPW));
+    }
+
+    return false;
+}
+
+module.exports {
+    getAllTextures,
+    isAuthenticated
+}
